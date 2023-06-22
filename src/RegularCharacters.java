8c9e2f2a1528e3e5381882468d04d2be604b4762
MrBarabulka <brother@ua.fm>
2023-06-23 01:45:37 +0300

    TextMenu added. RegularCharacters added.

---
+++ src/RegularCharacters.java
@@ -0,0 +1,8 @@
+public class RegularCharacters {
+    public static final String ENGLISH_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    public static final String CYRILLIC_ALPHABET = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+    public static final String PUNCTUATION_MARKS = ".?!,;:-()\"";
+
+    private RegularCharacters() {
+    }
+}
--- src/TextMenu.java
+++ src/TextMenu.java
@@ -3,5 +3,22 @@ public class TextMenu {
     private static final int DECRYPT_FILE = 1;
     private static final int EXIT_APPLICATION = 2;
     private static final String INCORRECT_PATHFILE = "Path file is incorrect, please enter it again!";
+    private static final String FILE_DOES_NOT_EXIST = "File does not exist, please check.";
+    private static final String ERROR_MENU_SELECTION = "Wrong number! Select the menu item again!";
+
+    private static final String TEXT_MENU =
+            """
+                         ┌────────────────────────┐
+                        ┌────────────────────────┐│
+                        │ Menu:                  ││
+                        ├────────────────────────┤│
+                        │ 0: To Encrypt File     ││
+                        ├────────────────────────┤│
+                        │ 1: To Decrypt File     ││
+                        ├────────────────────────┤│
+                        │ 2: To Exit Application │┘
+                        └────────────────────────┘
+            """;
+
 
 }
