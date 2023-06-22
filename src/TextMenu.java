0c114df97e6b2f2040d34c7600f8ebcdf0860798
MrBarabulka <brother@ua.fm>
2023-06-23 00:22:42 +0300

    TextMenu added

--- src/TextMenu.java
+++ src/TextMenu.java
@@ -2,5 +2,6 @@ public class TextMenu {
     private static final int ENCRYPT_FILE = 0;
     private static final int DECRYPT_FILE = 1;
     private static final int EXIT_APPLICATION = 2;
+    private static final String INCORRECT_PATHFILE = "Path file is incorrect, pls enter it again!";
 
 }
