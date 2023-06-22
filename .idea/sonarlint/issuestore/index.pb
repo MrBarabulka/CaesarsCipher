ca303ab645f70d4b5176315cfa82ee7b256c9dd8
MrBarabulka <brother@ua.fm>
2023-06-22 23:57:44 +0300

    git init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+F
+src/CaesarsEngine.java,1\8\1842daf9ea12381df9e5d33253149cb771e35c12
---
+++ src/TextMenu.java
@@ -0,0 +1,6 @@
+public class TextMenu {
+    private static final int ENCRYPT_FILE = 0;
+    private static final int DECRYPT_FILE = 1;
+    private static final int EXIT_ = 1;
+
+}
