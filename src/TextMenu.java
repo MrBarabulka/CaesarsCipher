782b6d9372e0af2b9a3b788d984a2276d264e64e
MrBarabulka <brother@ua.fm>
2023-06-23 00:11:44 +0300

    TextMenu

--- src/TextMenu.java
+++ src/TextMenu.java
@@ -1,6 +1,6 @@
 public class TextMenu {
     private static final int ENCRYPT_FILE = 0;
     private static final int DECRYPT_FILE = 1;
-    private static final int EXIT_ = 1;
+    private static final int EXIT_APPLICATION = 2;
 
 }
