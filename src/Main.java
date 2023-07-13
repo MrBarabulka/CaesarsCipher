f59cbcc74d650094504e9047aceb8f0803d9e442
Mr. Barabulka <128928027+MrBarabulka@users.noreply.github.com>
2023-07-14 01:57:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         PrintToScreenEngine printToScreenEngine = new PrintToScreenEngine();
         TextMenu textMenu = new TextMenu(printToScreenEngine);
-        textMenu.textMenuStart();
+        textMenu.textMenuStart();    
     }
 }
