f2d67bcb1cec53941dcc849d5278b080a2a956c3
Mr. Barabulka <128928027+MrBarabulka@users.noreply.github.com>
2023-07-21 19:31:18 +0300

    Update PrintToScreenEngine.java

--- src/PrintToScreenEngine.java
+++ src/PrintToScreenEngine.java
@@ -1,12 +1,6 @@
 public class PrintToScreenEngine {
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_RESET = "\u001B[0m";
 
-    public void printTo(String report, boolean isRedColor) {
-        if (isRedColor) {
-            System.out.println(ANSI_RED + report + ANSI_RESET);
-        } else {
-            System.out.println(report);    
-        }
+    public void printTo(String report) {
+        System.out.println(report);
     }
 }
